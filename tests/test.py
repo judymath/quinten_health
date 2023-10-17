5a3d918c6c531839a76c7ab86a8eec70400a7fe4
tlepic <lepicth@gmail.com>
2023-10-17 22:50:30 +0200

    Push tests

---
+++ tests/test.py
@@ -0,0 +1,60 @@
+"""Test topic"""
+
+import pytest
+from health.topic import gpt_analyze
+
+
+# Create a dummy LLMModel for testing
+class DummyLLMModel:
+    """
+    Dummy class for testing
+    """
+
+    def __init__(self):
+        pass
+
+
+# Write test cases for gpt_analyze
+def test_gpt_analyze_with_valid_input():
+    """
+    test_gpt_analyze_with_valid_input
+    """
+    comment = "This is a test comment."
+    llm = DummyLLMModel()  # Use the dummy model for testing
+    analyze_prompt = "Analyze the following comment: {{comment}}"
+
+    result = gpt_analyze(comment, llm, analyze_prompt)
+
+    # Add assertions to validate the result based on the expected output
+    assert result  # Ensure the result is not empty or None
+    assert isinstance(result, str)  # Ensure the result is a string
+    assert (
+        "This is a test comment." in result
+    )  # Check if the comment is present in the result
+
+
+def test_gpt_analyze_with_empty_comment():
+    """
+    test_gpt_analyze_with_empty_comment
+    """
+    comment = ""
+    llm = DummyLLMModel()
+    analyze_prompt = "Analyze the following comment: {{comment}}"
+
+    result = gpt_analyze(comment, llm, analyze_prompt)
+
+    # Add assertions to validate the result based on the expected output
+    assert result  # Ensure the result is not empty or None
+    assert isinstance(result, str)  # Ensure the result is a string
+
+
+def test_gpt_analyze_with_invalid_model():
+    """
+    test_gpt_analyze_with_invalid_model
+    """
+    comment = "This is another test comment."
+    llm = None  # Simulate an invalid LLMModel
+    analyze_prompt = "Analyze the following comment: {{comment}}"
+
+    with pytest.raises(Exception):  # Expecting an exception to be raised
+        gpt_analyze(comment, llm, analyze_prompt)
