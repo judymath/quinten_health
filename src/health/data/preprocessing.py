9e69e4c245b70d791613cb9a981df2c430658066
tlepic <lepicth@gmail.com>
2023-10-17 17:39:57 +0200

    Add processing functions

--- src/health/data/preprocessing.py
+++ src/health/data/preprocessing.py
@@ -3,6 +3,7 @@ import string
 import warnings
 
 import numpy as np
+import pandas as pd
 import spacy
 from nltk.corpus import stopwords
 from textblob import TextBlob
@@ -86,3 +87,62 @@ def preprocess_data(data):
     )
 
     return data
+
+
+def preprocess_topic(df):
+    # Drop text_index column
+    df.drop(columns="text_index", inplace=True)
+
+    # Select only comments for Crohn's Disease and Ulcerative Colitis
+    df = df.loc[
+        df["medication"].str.contains("Crohn")
+        | df["medication"].str.contains("Ulcerative")
+    ]
+
+    # Drop rows where medication is not specified
+    df = df.loc[~df["medication"].str.contains("For")]
+
+    # Separate drug from disease
+    df["drug"] = df["medication"].str.split("for").str[0]
+    df["disease"] = np.where(
+        df["medication"].str.contains("Crohn"),
+        "Crohn's Disease",
+        np.where(df["medication"].str.contains("Ulcerative"), "Ulcerative Colitis", ""),
+    )
+    df.drop(columns="medication", inplace=True)
+
+    # Drop row n°3 (triggering OpenAI's policy)
+    y = df.iloc[3]
+    df = df.drop(3)
+
+    return df
+
+
+def postprocess_topic(df):
+    # Apply topic extraction function to each gpt_analysis
+    df["no_side_effects"] = df["topics"].str.contains(" 0|Ex") & ~df[
+        "topics"
+    ].str.contains(", 0|0,")
+    df["fatigue"] = (
+        df["topics"].str.contains("1") & ~df["topics"].str.contains("10|11|12|13|14")
+    ) | df["topics"].str.contains("1,")
+    df["diarrhea"] = (
+        df["topics"].str.contains("2") & ~df["topics"].str.contains("12")
+    ) | (df["topics"].str.contains("2,") & ~df["topics"].str.contains("12,"))
+    df["arthralgia"] = (
+        df["topics"].str.contains("3") & ~df["topics"].str.contains("13")
+    ) | (df["topics"].str.contains("3,") & ~df["topics"].str.contains("13,"))
+    df["headaches"] = (
+        df["topics"].str.contains("4") & ~df["topics"].str.contains("14")
+    ) | (df["topics"].str.contains("4,") & ~df["topics"].str.contains("14,"))
+    df["nausea"] = df["topics"].str.contains("5")
+    df["rash"] = df["topics"].str.contains("6")
+    df["hair loss"] = df["topics"].str.contains("7")
+    df["constipation"] = df["topics"].str.contains("8")
+    df["mental_health_issues"] = df["topics"].str.contains("9")
+    df["leg_cramps"] = df["topics"].str.contains("10")
+    df["heart_blood_pressure_issues"] = df["topics"].str.contains("11")
+    df["liver_kidney_pain"] = df["topics"].str.contains("12")
+    df["weight_loss"] = df["topics"].str.contains("13")
+    df["weight_gain"] = df["topics"].str.contains("14")
+    return df
